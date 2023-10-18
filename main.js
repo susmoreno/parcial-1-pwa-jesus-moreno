454bb2721e226810ff91fe3687d80b988d6340ba
susmoreno <sus.alejandror@gmail.com>
2023-10-17 22:16:42 -0300

    creación de promesas y conexión con API
    en este caso, he creado algunos methods para traer la información.
    
    he hecho un async para traer la información de la API otorgada, usando la condicion de status = alive en el mismo link.
    
    También he creado una promesa que me retorna el status del personaje en una ventana modal y he añadido la información de el name y la url del character en la card.

--- main.js
+++ main.js
@@ -2,17 +2,83 @@ const app = new Vue({
     el: '#app',
     data: {
         estadoModal: false,
-        pokemonActual: null,
+        personajeActual: null,
         titulo: 'Personaje de Rick and Morty',
         personajes: [],
         storage: []
     },
 
     methods: {
-
-        guardarPersonaje(data, url){
+        guardarLocalStorage() {
+            localStorage.setItem("personajes", JSON.stringify(this.personajes));
+        },
+        guardarPersonajeLocal(data, url) {
             const personaje = {...data, url};
-            const historial = JSON.parse(localStorage.getItem("storage"));
+            const storage = JSON.parse(localStorage.getItem("storage"));
+            if (storage) {
+                storage.push(personaje);
+                this.storage = storage;
+            } else {
+                this.storage.push(personaje);
+            }
+            localStorage.setItem("storage", JSON.stringify(this.storage));
+        },
+        obtenerPersonajeStorage(url) {
+            const storage = JSON.parse(localStorage.getItem("storage"));
+            if(storage) {
+                const personaje = storage.find(personaje => personaje.url === url);
+                if(personaje) {
+                    return personaje;
+                } else {
+                    return;
+                }
+            } else {
+                return;
+            }
+        },
+        obtenerLocalStorage() {
+            const personajes = localStorage.getItem("personajes");
+            const storage = localStorage.getItem("storage");
+            if (personajes) {
+                this.personajes = JSON.parse(personajes);
+            }
+            if (storage) {
+                this.storage = JSON.parse(storage);
+            }
+        },
+        mostrarModal() {
+            this.estadoModal = true;
+        },
+        ocultarModal() {
+            this.estadoModal = false;
+        },
+        verPersonaje(url) {
+            const personaje = this.obtenerPersonajeStorage(url);
+            if (personaje) {
+              this.personajeActual = personaje;
+              this.mostrarModal();
+            } else {
+              fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                  this.personajeActual = {
+                    status: data.status,
+                    species: data.species
+                  };
+                  this.guardarPersonajeLocal(this.personajeActual, url);
+                  this.mostrarModal();
+                });
+            }
+          }
+    },
+    async mounted() {
+        this.obtenerLocalStorage();
+        if(this.personajes.length === 0) {
+            const response = await fetch("https://rickandmortyapi.com/api/character/?page=1&status=alive");
+            const data = await response.json();
+            this.personajes = data.results;
+            this.guardarLocalStorage();
         }
     }
-})
+}
+);
